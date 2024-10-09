e3ac692c588b747874d5b6b57fc4b111fe2ef235
ATOROYO <davidatoroyosika@gmail.com>
2024-10-09 23:57:43 +0300

    Added another code

--- controllers/authControlller.js
+++ controllers/authControlller.js
@@ -4,4 +4,9 @@ const bcrypt = require("bcrypt");
 // User registration logic/ function
 exports.registerUser = async (req, res) => {
   const { name, email, password } = req.body;
-};
+
+  try{
+// Check if user exist in db
+const [row] =await db.execute("SELECT * FROM users WHERE email = ?" [email])
+  }
+}
