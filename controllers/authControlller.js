3ce01aeea8aee29c60c80b9da23a659e5b500b02
ATOROYO <davidatoroyosika@gmail.com>
2024-10-09 23:50:40 +0300

    Started writting code for registering new user

--- controllers/authControlller.js
+++ controllers/authControlller.js
@@ -1,2 +1,7 @@
 const db = require("../config/db");
 const bcrypt = require("bcrypt");
+
+// User registration logic/ function
+exports.registerUser = async (req, res) => {
+  const { name, email, password } = req.body;
+};
