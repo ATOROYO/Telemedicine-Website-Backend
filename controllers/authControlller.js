a1e642ca194d4b733d24740ceb2bb2d12b190b2d
ATOROYO <davidatoroyosika@gmail.com>
2024-10-10 17:58:17 +0300

    Wrote some code

--- controllers/authControlller.js
+++ controllers/authControlller.js
@@ -3,10 +3,13 @@ const bcrypt = require("bcrypt");
 
 // User registration logic/ function
 exports.registerUser = async (req, res) => {
-  const { name, email, password } = req.body;
+    const { name, email, password } = req.body;
 
-  try{
-// Check if user exist in db
-const [row] =await db.execute("SELECT * FROM users WHERE email = ?" [email])
+    try{
+  // Check if user exist in db
+  const [row] =await db.execute("SELECT * FROM users WHERE email = ?" ,[email])
+  if(row.lenth > 0){
+      return res.status(400).json({message:"User already exist"})
   }
+    }
 }
