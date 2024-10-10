d80d0f06412ce4dbbf11db4897748b3beb1c875a
ATOROYO <davidatoroyosika@gmail.com>
2024-10-10 18:12:29 +0300

    Wrote code to catch server error

--- controllers/authControlller.js
+++ controllers/authControlller.js
@@ -3,20 +3,29 @@ const bcrypt = require("bcrypt");
 
 // User registration logic/ function
 exports.registerUser = async (req, res) => {
-    const { name, email, password } = req.body;
+  const { name, email, password } = req.body;
 
-    try{
-  // Check if user exist in db
-  const [row] =await db.execute("SELECT * FROM users WHERE email = ?" ,[email])
-  if(row.lenth > 0){
-      return res.status(400).json({message:"User already exist"})
-  }
-
-//   Hash the password
-const hashedPassword = await bcrypt.hash(password,10)
-
-// Insert the record to db
-await db.execute("INSERT INTO users (name, email, password) VALUES (?,?,?)", [name, email ,hashedPassword])
-res.status(201).json({message:"Usser registered successfully."})
+  try {
+    // Check if user exist in db
+    const [row] = await db.execute("SELECT * FROM users WHERE email = ?", [
+      email,
+    ]);
+    if (row.lenth > 0) {
+      return res.status(400).json({ message: "User already exist" });
     }
-}
+
+    //   Hash the password
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // Insert the record to db
+    await db.execute(
+      "INSERT INTO users (name, email, password) VALUES (?,?,?)",
+      [name, email, hashedPassword]
+    );
+    res.status(201).json({ message: "Usser registered successfully." });
+
+    // Server error
+  } catch (err) {
+    res.status(500).json({ message: "An error occured" });
+  }
+};
